77ffcc823b7db5e60ab346ae002a8aa1392422fe
trebinor <trebinor@users.noreply.github.com>
2023-12-30 13:33:09 -0500

    Fixed 2019 day 18 part 2 input 4 test case using a new function that finds shortest path among all 4 bots moving independently while respecting which keys they have. Given enough time and memory this would find the shortest path for the puzzle input, but I'm still using the hacky version that ignores doors and adds each bot's shortest path. TODO: trim the maze quadrants to be possible paths between keys and find the shortest path in a reasonable amount of time without the hacky version.

--- 2019/rust/aoc_2019/src/day18.rs
+++ 2019/rust/aoc_2019/src/day18.rs
@@ -29,6 +29,11 @@ struct Point(i8, i8, u32);
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 struct Location(i8, i8);
 
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct QuadbotPoint {
+    quadbots: Vec<Point>,
+}
+
 impl Point {
     fn key_present(&self, key: char) -> bool {
         self.2 & (1 << (key as u8 - 97u8)) != 0
@@ -40,6 +45,17 @@ impl Point {
             _ => unreachable!(),
         }
     }
+
+    #[allow(dead_code)]
+    fn print_keys(&self) {
+        for i in 'a'..'z' {
+            if self.key_present(i) {
+                print!("{}", i);
+            }
+        }
+        println!();
+    }
+
     fn successors(&self, input: &Maze) -> Vec<Point> {
         let &Point(x, y, k) = self;
         let mut path = Vec::new();
@@ -64,6 +80,75 @@ impl Point {
                 _ => {}
             }
             path.push(Point(i, j, keys));
+        }
+        path
+    }
+}
+
+impl QuadbotPoint {
+    fn successors(&self, input: &Maze) -> Vec<QuadbotPoint> {
+        let mut path = Vec::new();
+        let QuadbotPoint { quadbots: qbs } = self;
+        for (bi, _b) in qbs.into_iter().enumerate() {
+            let Point(x, y, k) = qbs[bi];
+            for m in [(0, 1), (1, 0), (0, -1), (-1, 0)].iter() {
+                let i: i8 = x + m.0;
+                let j: i8 = y + m.1;
+                let mut keys = k;
+                let mut key_point = Point(0, 0, k);
+                match input[j as usize][i as usize] {
+                    '#' => continue,
+                    door @ 'A'..='Z' => {
+                        if !key_point.key_present(door.to_ascii_lowercase()) {
+                            continue;
+                        }
+                    }
+                    key @ 'a'..='z' => {
+                        keys = {
+                            key_point.add_key(key);
+                            key_point.2
+                        }
+                    }
+                    _ => {}
+                }
+                if bi == 0 {
+                    path.push(QuadbotPoint {
+                        quadbots: vec![
+                            Point(i, j, keys),
+                            Point(qbs[1].0, qbs[1].1, keys),
+                            Point(qbs[2].0, qbs[2].1, keys),
+                            Point(qbs[3].0, qbs[3].1, keys),
+                        ],
+                    });
+                } else if bi == 1 {
+                    path.push(QuadbotPoint {
+                        quadbots: vec![
+                            Point(qbs[0].0, qbs[0].1, keys),
+                            Point(i, j, keys),
+                            Point(qbs[2].0, qbs[2].1, keys),
+                            Point(qbs[3].0, qbs[3].1, keys),
+                        ],
+                    });
+                } else if bi == 2 {
+                    path.push(QuadbotPoint {
+                        quadbots: vec![
+                            Point(qbs[0].0, qbs[0].1, keys),
+                            Point(qbs[1].0, qbs[1].1, keys),
+                            Point(i, j, keys),
+                            Point(qbs[3].0, qbs[3].1, keys),
+                        ],
+                    });
+                } else if bi == 3 {
+                    path.push(QuadbotPoint {
+                        quadbots: vec![
+                            Point(qbs[0].0, qbs[0].1, keys),
+                            Point(qbs[1].0, qbs[1].1, keys),
+                            Point(qbs[2].0, qbs[2].1, keys),
+                            Point(i, j, keys),
+                        ],
+                    });
+                }
+            }
         }
         path
     }
@@ -111,7 +196,7 @@ pub fn shortest_path(input: &Maze) -> usize {
 }
 
 #[aoc(day18, part2)]
-pub fn shortest_path_with_quadbots(input: &Maze) -> usize {
+pub fn shortest_path_with_quadbots_ignore_doors(input: &Maze) -> usize {
     let mut quadmaze = input.clone();
     let mut goal = Point(0, 0, 0);
     let mut origin_x: i8 = 0;
@@ -134,9 +219,6 @@ pub fn shortest_path_with_quadbots(input: &Maze) -> usize {
             }
         }
     }
-    // println!("origin: {} {}", origin_x, origin_y);
-    // let h = input.len();
-    // let w = input[0].len();
     let mut bots: [Point; 4] = [Point(0, 0, 0); 4];
     for (idx, o) in [(-1, -1), (1, -1), (1, 1), (-1, 1)].iter().enumerate() {
         let i = origin_x as i8 + o.0;
@@ -180,11 +262,64 @@ pub fn shortest_path_with_quadbots(input: &Maze) -> usize {
     paths.iter().map(|v| v.len() - 1).sum()
 }
 
+pub fn shortest_path_with_quadbots(input: &Maze) -> usize {
+    let mut quadmaze = input.clone();
+    let mut goal = Point(0, 0, 0);
+    let mut origin_x: i8 = 0;
+    let mut origin_y: i8 = 0;
+    let mut key_points: Vec<Point> = Vec::new();
+    for (j, y) in input.iter().enumerate() {
+        for (i, _x) in y.iter().enumerate() {
+            match input[j][i] {
+                '@' => {
+                    origin_x = i as i8;
+                    origin_y = j as i8;
+                }
+                key @ 'a'..='z' => {
+                    goal.add_key(key);
+                    let mut p = Point(i as i8, j as i8, 0);
+                    p.add_key(key);
+                    key_points.push(p);
+                }
+                _ => {}
+            }
+        }
+    }
+    let mut bots: [Point; 4] = [Point(0, 0, 0); 4];
+    for (idx, o) in [(-1, -1), (1, -1), (1, 1), (-1, 1)].iter().enumerate() {
+        let i = origin_x as i8 + o.0;
+        let j = origin_y as i8 + o.1;
+        bots[idx] = Point(i, j, 0);
+        quadmaze[j as usize][i as usize] = '@'
+    }
+    for o in [(0, 0), (-1, 0), (0, 1), (1, 0), (0, -1)].iter() {
+        let i = origin_x as i8 + o.0;
+        let j = origin_y as i8 + o.1;
+        quadmaze[j as usize][i as usize] = '#'
+    }
+
+    let quadbots = QuadbotPoint {
+        quadbots: vec![bots[0], bots[1], bots[2], bots[3]],
+    };
+    let path = pathfinding::directed::bfs::bfs(
+        &quadbots,
+        |p| p.successors(&quadmaze),
+        |p| {
+            p.quadbots[0].2 == goal.2
+                && p.quadbots[1].2 == goal.2
+                && p.quadbots[2].2 == goal.2
+                && p.quadbots[3].2 == goal.2
+        },
+    );
+    path.unwrap().len() - 1
+}
+
 #[cfg(test)]
 mod tests {
     use crate::day18::generator;
     use crate::day18::shortest_path;
     use crate::day18::shortest_path_with_quadbots;
+    use crate::day18::shortest_path_with_quadbots_ignore_doors;
     use std::fs;
     const ANSWER_18A: usize = 4118;
     const ANSWER_18B: usize = 1828;
@@ -269,7 +404,7 @@ mod tests {
     fn t18b() {
         assert_eq!(
             ANSWER_18B,
-            shortest_path_with_quadbots(&generator(
+            shortest_path_with_quadbots_ignore_doors(&generator(
                 &fs::read_to_string("input/2019/day18.txt").unwrap().trim()
             ))
         );
